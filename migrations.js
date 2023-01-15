4c03ac33926e24455930ae7a07dd3eb7bb2911c1
NguyenTienDung21 <dung.tnguyen@katalon.com>
2023-01-15 08:47:44 +0700

    uppdate

--- migrations.js
+++ migrations.js
@@ -3,11 +3,11 @@ const {pool} = require('./models/pool')
 
 async function createTable(){
     try {
-        const res = await pool.query("CREATE TABLE my_number (number INT)");
+        const res2 = await pool.query("INSERT INTO my_number VALUES 9")
         return res.rows
       } catch (error) {
         console.error(error);
       }
 }
 
-createTable()
+console.log(createTable())
